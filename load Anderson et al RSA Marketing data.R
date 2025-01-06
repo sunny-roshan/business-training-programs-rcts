fbc58e3aaf397514f78b72726338046f1cf109e0
sunny-roshan <sunny.roshan99@gmail.com>
2025-01-06 19:12:27 +0000

    minor cleaning and commenting of load Anderson et al RSA Marketing data.R

--- load Anderson et al RSA Marketing data.R
+++ load Anderson et al RSA Marketing data.R
@@ -1,24 +1,30 @@
+# Load libraries
 library(dplyr)
 library(tidyr)
 library(haven)
 install.packages("labelled")
 library(labelled)
 
-RSA <- read_dta("/Users/sunny/Library/CloudStorage/OneDrive-Personal/Documents/MPhil/Thesis/Thesis/Data for thesis/Business Training Data available/Anderson et al South Africa/MS Data Folder -- Pathways to Profits/P2P_dataset.dta")
-View(RSA)
+# Set path
+## Set loc
 
+# Load dataset
+RSA <- read_dta("/Users/sunny/Library/CloudStorage/OneDrive-Personal/Documents/MPhil/Thesis/Thesis/Data for thesis/Business Training Data available/Anderson et al South Africa/MS Data Folder -- Pathways to Profits/P2P_dataset.dta")
+
+# Check treatments and treatment status
 table(RSA$Treatment_FIN)
 table(RSA$Treatment_MKT)
 
+# Keep only marketing treatment; separate df for finance treatment for comparison
 RSA_Mkt <- filter(RSA, Treatment_FIN == 0)
 
+# Check missing data for sales and profits
 sum(is.na(RSA_Mkt$Profits3_composite_w1))
 sum(is.na(RSA_Mkt$Sales5_composite_w1))
 sum(is.na(RSA_Fin$Profits3_composite_w1))
 sum(is.na(RSA_Fin$Sales5_composite_w1))
 
-##### 
-##### Marketing treatment first
+# Standardise data
 Standardisation_vars_RSA_Mkt <- group_by(RSA_Mkt, T_survey_round, Treatment_MKT) %>% 
   summarize(avgprofits = mean(Profits3_composite_w1, na.rm = TRUE), sdprofits = sd(Profits3_composite_w1, na.rm = TRUE),
             avgrevenue = mean(Sales5_composite_w1, na.rm = TRUE), sdrevenue = sd(Sales5_composite_w1, na.rm = TRUE))
@@ -39,32 +45,26 @@ View(RSA_Mkt_clean)
 RSA_Mkt_clean <- RSA_Mkt_clean %>% mutate(Profit_zscore = (Profits3_composite_w1 - avgprofits)/sdprofits,
                                       Revenue_zscore = (Sales5_composite_w1 - avgrevenue)/sdrevenue)
 
-##
-# Now make a cleaner variable:
-# Collapse both follow-up survey rounds into a single follow-up indicator, for higher power;
-# and add the baseline values as a column for each individual rather than a row entry.
+# Cleaner data:
+# 1. collapse both follow-up survey rounds into a single follow-up indicator, for higher power;
+# 2. add the baseline values as a column for each individual rather than a row entry;
 RSA_Mkt_clean <- RSA_Mkt_clean %>% mutate(fup_indicator = as.numeric(T_survey_round != 1))
 RSA_Mkt_baseline <- filter(RSA_Mkt_clean, fup_indicator == 0)
 RSA_Mkt_clean <- filter(RSA_Mkt_clean, fup_indicator == 1)
 
-View(RSA_Mkt_baseline)
-RSA_Mkt_baseline <- rename(RSA_Mkt_baseline, Baseline_Profit_z = Profit_zscore, Baseline_Revenue_z = Revenue_zscore)
-RSA_Mkt_baseline <- RSA_Mkt_baseline %>% select(N_firm_id, Baseline_Profit_z, Baseline_Revenue_z)
+RSA_Mkt_baseline <- RSA_Mkt_baseline %>% 
+  rename(Baseline_Profit_z = Profit_zscore, Baseline_Revenue_z = Revenue_zscore) %>% 
+  select(N_firm_id, Baseline_Profit_z, Baseline_Revenue_z)
 
-# Now we can drop most columns - they may be used as controls to replicate the regressions, if you want to use
-# the Bandiera et al. method; but if we want to combine the datasets' individual observations in the 
-# Meager (2019) method, the controls won't be the same across datasets so probably droppable.
-RSA_Mkt_clean <- RSA_Mkt_clean %>% inner_join(RSA_Mkt_baseline, by="N_firm_id")
-RSA_Mkt_clean <- rename(RSA_Mkt_clean, treatment_indicator = Treatment_MKT, female = Gender)
-RSA_Mkt_clean <- RSA_Mkt_clean %>% select(treatment_indicator,
-                                      Profit_zscore, Revenue_zscore,
-                                      Baseline_Profit_z, Baseline_Revenue_z,
-                                      female)
-RSA_Mkt_clean <- RSA_Mkt_clean %>% mutate(urban = 1, group = "RSA Marketing")
-RSA_Mkt_clean <- RSA_Mkt_clean %>% relocate(female, .after = urban)
+RSA_Mkt_clean <- RSA_Mkt_clean %>% 
+  inner_join(RSA_Mkt_baseline, by="N_firm_id") %>%
+  rename(treatment_indicator = Treatment_MKT, female = Gender) %>%
+  mutate(urban = 1, group = "RSA Marketing") %>%
+  relocate(female, .after = urban) %>%
+  select(treatment_indicator, Profit_zscore, Revenue_zscore, Baseline_Profit_z, Baseline_Revenue_z, female)
+
 table(RSA_Mkt_clean$treatment_indicator)
 View(RSA_Mkt_clean)
 
-#export df
+# export df
 write.table(RSA_Mkt_clean, file = "/Users/sunny/Library/CloudStorage/OneDrive-Personal/Documents/MPhil/Thesis/Thesis/R/RSA_Market.csv", sep=",", row.names=FALSE)
-
