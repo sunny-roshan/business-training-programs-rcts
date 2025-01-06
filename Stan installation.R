377dd012539718349f8f046f928a7360a234537c
sunny-roshan <sunny.roshan99@gmail.com>
2025-01-06 12:34:12 +0000

    comment and reorganise Stan installation

--- Stan installation.R
+++ Stan installation.R
@@ -1,14 +1,20 @@
+# Install rstan package and its dependencies
 install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
-example(stan_model, package = "rstan", run.dontrun = TRUE)
 
-## you must run these 3 commands every time
+# Load rstan and set options (run in each script)
 library(rstan)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
+# Install and load cmdstanr
 install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 library(cmdstanr)
 check_cmdstan_toolchain()
 cmdstanr::install_cmdstan()
+
+# Install additional packages
 install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"))
 devtools::install_github("rmcelreath/rethinking")
+
+# Example usage of stan_model from rstan
+example(stan_model, package = "rstan", run.dontrun = TRUE)
