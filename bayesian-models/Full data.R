e5806c87a32ba5febd7164883d1a77f2f1773904
sunny-rr <sunny.roshan99@gmail.com>
2025-01-14 12:48:08 +0000

    tidy code and update data paths

--- bayesian-models/Full data.R
+++ bayesian-models/Full data.R
@@ -1,47 +1,67 @@
+# here we run Bayesian hierarchical models on the combined datasets for the 6 studies for which we have access to all observations
+
+# Load packages
 library(baggr)
-vignette('baggr')
 library(xtable)
-options(xtable.floating = FALSE)
-options(xtable.timestamp = "")
 library(ggplot2)
 library(rstan)
 library(stargazer)
 library(dplyr)
 library(scales)
-install.packages('extraDistr')
 library(extraDistr)
 
+# Set options
+options(xtable.floating = FALSE)
+options(xtable.timestamp = "")
+
 # Set loc
-##
+getwd()
 
-#Load profits df with no NAs
-#Load sales df with no NAs
+# Load profits df with no NAs
+master_noprofna <- read.csv(here("cleaned-datasets", "Profits master dataset.csv"))
 
-Full <- baggr(data = Master_noprofna, model = "rubin_full",
-      iter = 15000, chains = 8, control = list(adapt_delta = 0.95),
-      pooling = "partial", prior_hypermean = normal(0,100),
-      prior_hypersd = cauchy(0,25), ppd = FALSE, pooling_control = "partial",
-      covariates = c("urban", "female"),
-      outcome = "Profit_zscore", group = "group",
-      treatment = "treatment_indicator")
+# Load sales df with no NAs
+master_nosalesna <- read.csv(here("cleaned-datasets", "Sales master dataset.csv"))
 
-# this model took over 20 minutes to run!!
-## Sales - 
-Full_sales <- baggr(data = Master_nosalesna, model = "rubin_full",
-              iter = 15000, chains = 8, control = list(adapt_delta = 0.95),
-              pooling = "partial", prior_hypermean = normal(0,100),
-              prior_hypersd = cauchy(0,25), 
-              prior_control = normal(0.000, 0.071^2),
-              prior_sigma = uniform(0, 76),
-              prior_beta = normal(0.0, 4.9^2),
-              covariates = c("urban", "female"),
-              outcome = "Revenue_zscore", group = "group",
-              treatment = "treatment_indicator")
-# Not a reliable model!
-# pairs(Full$fit, pars = c("mu", "theta_k"))
+# Profits full BHM (this model took over 20 minutes to run)
+full_profits <- baggr(
+  data = master_noprofna,
+  model = "rubin_full",
+  iter = 15000,
+  chains = 8,
+  control = list(adapt_delta = 0.95),
+  pooling = "partial",
+  prior_hypermean = normal(0, 100),
+  prior_hypersd = cauchy(0, 25),
+  ppd = FALSE,
+  pooling_control = "partial",
+  covariates = c("urban", "female"),
+  outcome = "Profit_zscore",
+  group = "group",
+  treatment = "treatment_indicator"
+)
 
-Full_sales$fit
-#* control [independent prior on control means] ~ normal(0.000, 0.071^2)
-#* sigma [error term in linear regresion] ~ uniform(0, 76)
-#Setting prior for covariates in regression to normal, with SD equal to 10*(highest SD among covariates):
-# * beta ~ normal(0.0, 4.9^2)---purely experimental, we recommend you set this manually
+# Check
+pairs(full_profits$fit, pars = c("mu", "theta_k"))
+
+# Profits full BHM (this model took over 20 minutes to run)
+full_sales <- baggr(
+  data = master_nosalesna,
+  model = "rubin_full",
+  iter = 15000,
+  chains = 8,
+  control = list(adapt_delta = 0.95),
+  pooling = "partial",
+  prior_hypermean = normal(0, 100),
+  prior_hypersd = cauchy(0, 25),
+  prior_control = normal(0.000, 0.071 ^ 2),  # Setting prior for covariates in regression to normal, with SD equal to 10*(highest SD among covariates)
+  prior_sigma = uniform(0, 76),
+  prior_beta = normal(0.0, 4.9 ^ 2),  # experimental
+  covariates = c("urban", "female"),
+  outcome = "Revenue_zscore",
+  group = "group",
+  treatment = "treatment_indicator"
+)
+
+# Check
+full_sales$fit
