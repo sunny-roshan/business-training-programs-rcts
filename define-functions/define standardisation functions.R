3d2bb3961bf84a592157ba12551ad6812884da45
sunny-rr <sunny.roshan99@gmail.com>
2025-01-14 12:41:43 +0000

    tidy code

--- define-functions/define standardisation functions.R
+++ define-functions/define standardisation functions.R
@@ -2,7 +2,12 @@
 # allowing for baseline revenues/profits to be a separate column
 
 # 1. Define a function to calculate means and standard deviations for control group
-standardize_data <- function(data, treatment_col, sales_col, profits_col, baseline_sales_col = NULL, baseline_profits_col = NULL) {
+standardise_data <- function(data,
+                             treatment_col,
+                             sales_col,
+                             profits_col,
+                             baseline_sales_col = NULL,
+                             baseline_profits_col = NULL) {
   summarization <- data %>%
     group_by(!!sym(treatment_col)) %>%
     summarize(
@@ -12,7 +17,8 @@ standardize_data <- function(data, treatment_col, sales_col, profits_col, baseli
       sdrevenue = sd(!!sym(sales_col), na.rm = TRUE)
     )
   
-  if (!is.null(baseline_sales_col) && baseline_sales_col %in% colnames(data)) {
+  if (!is.null(baseline_sales_col) &&
+      baseline_sales_col %in% colnames(data)) {
     summarization <- summarization %>%
       mutate(
         avgbaselinerevenue = mean(!!sym(baseline_sales_col), na.rm = TRUE),
@@ -20,7 +26,8 @@ standardize_data <- function(data, treatment_col, sales_col, profits_col, baseli
       )
   }
   
-  if (!is.null(baseline_profits_col) && baseline_profits_col %in% colnames(data)) {
+  if (!is.null(baseline_profits_col) &&
+      baseline_profits_col %in% colnames(data)) {
     summarization <- summarization %>%
       mutate(
         avgbaselineprofits = mean(!!sym(baseline_profits_col), na.rm = TRUE),
@@ -30,25 +37,27 @@ standardize_data <- function(data, treatment_col, sales_col, profits_col, baseli
 }
 
 # 2. Define a function to calculate z-scores
-calculate_z_scores <- function(data, sales_col, profits_col, baseline_sales_col = NULL, baseline_profits_col = NULL) {
+calculate_z_scores <- function(data,
+                               sales_col,
+                               profits_col,
+                               baseline_sales_col = NULL,
+                               baseline_profits_col = NULL) {
   data <- data %>%
     mutate(
       Profit_zscore = (!!sym(profits_col) - avgprofits) / sdprofits,
       Revenue_zscore = (!!sym(sales_col) - avgrevenue) / sdrevenue
     )
   
-  if (!is.null(baseline_sales_col) && baseline_sales_col %in% colnames(data)) {
+  if (!is.null(baseline_sales_col) &&
+      baseline_sales_col %in% colnames(data)) {
     data <- data %>%
-      mutate(
-        Baseline_Revenue_z = (!!sym(baseline_sales_col) - avgbaselinerevenue) / sdbaselinerevenue
-      )
+      mutate(Baseline_Revenue_z = (!!sym(baseline_sales_col) - avgbaselinerevenue) / sdbaselinerevenue)
   }
   
-  if (!is.null(baseline_profits_col) && baseline_profits_col %in% colnames(data)) {
+  if (!is.null(baseline_profits_col) &&
+      baseline_profits_col %in% colnames(data)) {
     data <- data %>%
-      mutate(
-        Baseline_Profit_z = (!!sym(baseline_profits_col) - avgbaselineprofits) / sdbaselineprofits
-      )
+      mutate(Baseline_Profit_z = (!!sym(baseline_profits_col) - avgbaselineprofits) / sdbaselineprofits)
   }
   
   data  # Explicitly return the modified data frame
